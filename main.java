0e7e65fe9126a4fbe300ae95139effcaf162db5f
toto <toto@gmail.com>
2017-09-12 12:29:24 +0200

    correction ano prod 2

--- main.java
+++ main.java
@@ -2,6 +2,7 @@ public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       // ajout commentaire
-      System.out.println("Hello, World");
+	//  ano 2
+      System.out.println("Hello World, this is test");
    }
 }
